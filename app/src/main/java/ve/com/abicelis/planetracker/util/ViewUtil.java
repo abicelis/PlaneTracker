507b4733fc3195e6e03dae333fb7aab65a35a8c8
abicelis <abicelis@gmail.com>
2017-08-29 14:00:10 -0400

    Handy ViewUtil for converting dp to px, etc.

---
+++ app/src/main/java/ve/com/abicelis/planetracker/util/ViewUtil.java
@@ -0,0 +1,60 @@
+ package ve.com.abicelis.planetracker.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by abicelis on 29/8/2017.
+ */
+
+public class ViewUtil {
+
+    /**
+     * This method converts dp unit to equivalent pixels, depending on device density.
+     *
+     * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
+     * @return A float value to represent px equivalent to dp depending on device density
+     */
+    public static float convertDpToPixel(float dp){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+
+        float px = dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px;
+    }
+
+    /**
+     * This method converts device specific pixels to density independent pixels.
+     *
+     * @param px A value in px (pixels) unit. Which we need to convert into db
+     * @return A float value to represent dp equivalent to px value
+     */
+    public static float convertPixelsToDp(float px){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+
+        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return dp;
+    }
+
+
+    /**
+     * This method returns the device height in px.
+     *
+     * @return A float value to represent screen height in px
+     */
+    public static float getScreenHeight(){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        return metrics.heightPixels;
+    }
+
+    /**
+     * This method hides the soft keyboard, given an activity
+     */
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm =
+                (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+    }
+}

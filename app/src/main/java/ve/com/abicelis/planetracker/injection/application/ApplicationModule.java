d6c38521baafe914d3fd87186dbb9b57944705d3
abicelis <abicelis@gmail.com>
2017-08-30 13:10:32 -0400

    Injection graph for Room db

--- app/src/main/java/ve/com/abicelis/planetracker/injection/application/ApplicationModule.java
+++ app/src/main/java/ve/com/abicelis/planetracker/injection/application/ApplicationModule.java
@@ -1,9 +1,14 @@
 package ve.com.abicelis.planetracker.injection.application;
 
 import android.app.Application;
+import android.arch.persistence.room.Room;
+
+import javax.inject.Named;
 
 import dagger.Module;
 import dagger.Provides;
+import ve.com.abicelis.planetracker.application.Constants;
+import ve.com.abicelis.planetracker.data.local.AppDatabase;
 
 /**
  * Created by abicelis on 28/8/2017.
@@ -20,5 +25,25 @@ public class ApplicationModule {
     @ApplicationScope
     Application application() { return mApplication; }
 
+
+
+
+
+
+    /**
+     * INJECTION GRAPH FOR ROOM DATABASE
+     */
+    private static final String ROOM_DATABASE_NAME = "ROOM_DATABASE_NAME";
+
+    @Provides
+    @Named(ROOM_DATABASE_NAME)
+    String provideRoomDatabaseName() {return Constants.ROOM_DATABASE_NAME;}
+
+    @Provides
+    @ApplicationScope
+    AppDatabase provideRoomAppDatabase(Application applicationContext, @Named(ROOM_DATABASE_NAME) String databaseName) {
+        return Room.databaseBuilder(applicationContext,
+                AppDatabase.class, databaseName).build();
+    }
 
 }

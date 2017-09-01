90cb88416bfc5623884770eb845347a4d4f6468e
abicelis <abicelis@gmail.com>
2017-08-31 22:46:06 -0400

    Moved room injection to its own Module, LocalModule

--- app/src/main/java/ve/com/abicelis/planetracker/injection/application/ApplicationModule.java
+++ app/src/main/java/ve/com/abicelis/planetracker/injection/application/ApplicationModule.java
@@ -1,14 +1,10 @@
 package ve.com.abicelis.planetracker.injection.application;
 
 import android.app.Application;
-import android.arch.persistence.room.Room;
-
-import javax.inject.Named;
+import android.content.Context;
 
 import dagger.Module;
 import dagger.Provides;
-import ve.com.abicelis.planetracker.application.Constants;
-import ve.com.abicelis.planetracker.data.local.AppDatabase;
 
 /**
  * Created by abicelis on 28/8/2017.
@@ -25,26 +21,8 @@ public class ApplicationModule {
     @ApplicationScope
     Application application() { return mApplication; }
 
-
-
-
-
-
-    /**
-     * INJECTION GRAPH FOR ROOM DATABASE
-     */
-    private static final String ROOM_DATABASE_NAME = "ROOM_DATABASE_NAME";
-
-    @Provides
-    @Named(ROOM_DATABASE_NAME)
-    String provideRoomDatabaseName() {return Constants.ROOM_DATABASE_NAME;}
-
     @Provides
     @ApplicationScope
-    AppDatabase provideRoomAppDatabase(Application applicationContext, @Named(ROOM_DATABASE_NAME) String databaseName) {
-        return Room.databaseBuilder(applicationContext,
-                AppDatabase.class, databaseName).fallbackToDestructiveMigration().build();
-        //TODO remember to kill fallbackToDestructiveMigration() and provide a proper migration plan on prod!
-    }
+    Context context() { return mApplication.getApplicationContext(); }
 
 }
---
+++ app/src/main/java/ve/com/abicelis/planetracker/injection/application/LocalModule.java
@@ -0,0 +1,38 @@
+package ve.com.abicelis.planetracker.injection.application;
+
+import android.app.Application;
+import android.arch.persistence.room.Room;
+
+import javax.inject.Named;
+
+import dagger.Module;
+import dagger.Provides;
+import ve.com.abicelis.planetracker.application.Constants;
+import ve.com.abicelis.planetracker.data.local.AppDatabase;
+
+/**
+ * Created by abicelis on 28/8/2017.
+ */
+
+@Module
+public class LocalModule {
+
+    private static final String ROOM_DATABASE_NAME = "ROOM_DATABASE_NAME";
+
+    /**
+     * INJECTION GRAPH FOR ROOM DATABASE
+     */
+
+    @Provides
+    @Named(ROOM_DATABASE_NAME)
+    String provideRoomDatabaseName() {return Constants.ROOM_DATABASE_NAME;}
+
+    @Provides
+    @ApplicationScope
+    AppDatabase provideRoomAppDatabase(Application applicationContext, @Named(ROOM_DATABASE_NAME) String databaseName) {
+        return Room.databaseBuilder(applicationContext,
+                AppDatabase.class, databaseName).fallbackToDestructiveMigration().build();
+        //TODO remember to kill fallbackToDestructiveMigration() and provide a proper migration plan on prod!
+    }
+
+}

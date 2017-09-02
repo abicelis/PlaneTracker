2230a683b4825017e2be8be5b568e484da39c629
abicelis <abicelis@gmail.com>
2017-09-01 21:09:57 -0400

    Changed observables to maybes, since observables are useless here. api either responds with an item, no item, or errors out.

--- app/src/main/java/ve/com/abicelis/planetracker/data/remote/FlightawareApi.java
+++ app/src/main/java/ve/com/abicelis/planetracker/data/remote/FlightawareApi.java
@@ -1,7 +1,6 @@
 package ve.com.abicelis.planetracker.data.remote;
 
 import io.reactivex.Maybe;
-import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import ve.com.abicelis.planetracker.data.model.flightaware.AirlineFlightSchedulesResponse;
@@ -12,16 +11,15 @@ import ve.com.abicelis.planetracker.data.model.flightaware.AirlineFlightSchedule
 
 public interface FlightawareApi {
 
-    //TODO change this Call<AirlineFlightSchedulesResponse> to retrofit, Observable<AirlineFlightSchedulesResponse>
     @GET("AirlineFlightSchedules")
-    Observable<AirlineFlightSchedulesResponse> getFlightSchedulesByRoute (@Query("start_date") String startDate,
-                                                                          @Query("end_date") String endDate,
-                                                                          @Query("origin") String origin,
-                                                                          @Query("destination") String destination);
+    Maybe<AirlineFlightSchedulesResponse> getFlightSchedulesByRoute (@Query("start_date") String startDate,
+                                                                     @Query("end_date") String endDate,
+                                                                     @Query("origin") String origin,
+                                                                     @Query("destination") String destination);
 
     @GET("AirlineFlightSchedules")
-    Maybe<AirlineFlightSchedulesResponse> getFlightScheduleByFlight (@Query("start_date") String startDate,
-                                                                     @Query("end_date") String endDate,
-                                                                     @Query("airline") String airline,
-                                                                     @Query("flightno") String flightNumber);
+    Maybe<AirlineFlightSchedulesResponse> getFlightScheduleByFlightNumber (@Query("start_date") String startDate,
+                                                                           @Query("end_date") String endDate,
+                                                                           @Query("airline") String airline,
+                                                                           @Query("flightno") int flightNumber);
 }

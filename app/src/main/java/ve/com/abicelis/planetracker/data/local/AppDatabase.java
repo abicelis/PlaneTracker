686edea450c8341505b6854701db10c4fefd7336
abicelis <abicelis@gmail.com>
2017-09-02 23:46:48 -0400

    Added Flight.class, Trip.class, their daos and some converters to room db.

--- app/src/main/java/ve/com/abicelis/planetracker/data/local/AppDatabase.java
+++ app/src/main/java/ve/com/abicelis/planetracker/data/local/AppDatabase.java
@@ -2,16 +2,22 @@ package ve.com.abicelis.planetracker.data.local;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 
 import ve.com.abicelis.planetracker.data.model.Airline;
 import ve.com.abicelis.planetracker.data.model.Airport;
+import ve.com.abicelis.planetracker.data.model.Flight;
+import ve.com.abicelis.planetracker.data.model.Trip;
 
 /**
  * Created by abicelis on 30/8/2017.
  */
 
-@Database(entities = {Airport.class, Airline.class}, version = 4)
+@Database(entities = {Airport.class, Airline.class, Flight.class, Trip.class}, version = 5)
+@TypeConverters({CalendarConverter.class, TripStatusConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AirportDao airportDao();
     public abstract AirlineDao airlineDao();
+    public abstract FlightDao flightDao();
+    public abstract TripDao tripDao();
 }

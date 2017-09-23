4ba3fc2bc1003827f15ef0da944363764459a546
abicelis <abicelis@gmail.com>
2017-09-22 20:01:27 -0400

    setDim() implementation on customEditText and full implementation of customTextView

--- app/src/main/java/ve/com/abicelis/planetracker/ui/customviews/CustomTextView.java
+++ app/src/main/java/ve/com/abicelis/planetracker/ui/customviews/CustomTextView.java
@@ -2,7 +2,6 @@ package ve.com.abicelis.planetracker.ui.customviews;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.support.annotation.ColorRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
@@ -17,6 +16,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,24 +26,24 @@ import ve.com.abicelis.planetracker.R;
  * Created by abicelis on 20/9/2017.
  */
 
-public class CustomEditText extends LinearLayout {
+public class CustomTextView extends LinearLayout {
 
 
-    @BindView(R.id.custom_edittext_icon)
+    @BindView(R.id.custom_textview_icon)
     ImageView mIcon;
 
-    @BindView(R.id.custom_edittext_text)
-    EditText mEditText;
+    @BindView(R.id.custom_textview_text)
+    TextView mTextView;
 
-    public CustomEditText(Context context) {
+    public CustomTextView(Context context) {
         super(context);
         init(context, null);
     }
-    public CustomEditText(Context context, @Nullable AttributeSet attrs) {
+    public CustomTextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context, attrs);
     }
-    public CustomEditText(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public CustomTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context, attrs);
     }
@@ -52,18 +52,18 @@ public class CustomEditText extends LinearLayout {
 
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.custom_edittext, this);
+        inflater.inflate(R.layout.custom_textview, this);
 
         ButterKnife.bind(this);
 
         //Get/apply custom xml configs
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.custom_edit_text);
-        int hintColor = a.getResourceId(R.styleable.custom_edit_text_hintcolor, -1);
-        int textColor = a.getResourceId(R.styleable.custom_edit_text_textcolor, -1);
-        int iconId = a.getResourceId(R.styleable.custom_edit_text_icon, -1);
-        int textId = a.getResourceId(R.styleable.custom_edit_text_text, -1);
-        int hintId = a.getResourceId(R.styleable.custom_edit_text_hint, -1);
-        int maxLines = a.getInt(R.styleable.custom_edit_text_maxlines, -1);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.custom_text_view);
+        int hintColor = a.getResourceId(R.styleable.custom_text_view_hintcolor, -1);
+        int textColor = a.getResourceId(R.styleable.custom_text_view_textcolor, -1);
+        int iconId = a.getResourceId(R.styleable.custom_text_view_icon, -1);
+        int textId = a.getResourceId(R.styleable.custom_text_view_text, -1);
+        int hintId = a.getResourceId(R.styleable.custom_text_view_hint, -1);
+        int maxLines = a.getInt(R.styleable.custom_text_view_maxlines, -1);
 
 
         setHintColor(hintColor);
@@ -80,21 +80,17 @@ public class CustomEditText extends LinearLayout {
         this.setBackgroundResource(R.drawable.round_edges_background);
     }
 
-    public EditText getInternalEditText(){
-        return mEditText;
-    }
-
     public void setHintColor(@ColorRes int colorRes) {
         if (colorRes != -1) {
             int color = ContextCompat.getColor(getContext(), colorRes);
-            mEditText.setHintTextColor(color);
+            mTextView.setHintTextColor(color);
         }
     }
 
     public void setTextColor(int colorRes) {
         if (colorRes != -1) {
             int color = ContextCompat.getColor(getContext(), colorRes);
-            mEditText.setTextColor(color);
+            mTextView.setTextColor(color);
         }
     }
 
@@ -108,70 +104,50 @@ public class CustomEditText extends LinearLayout {
         }
     }
 
-    public void setMaxLength(int length) {
-        mEditText.setFilters( new InputFilter[] { new InputFilter.LengthFilter(length) } );
-    }
 
     public void setMaxLines(int maxLines) {
         if(maxLines != -1) {
-            mEditText.setMaxLines(maxLines);
+            mTextView.setMaxLines(maxLines);
             if(maxLines == 1)
-                mEditText.setSingleLine();
+                mTextView.setSingleLine();
         }
     }
 
-    public void setInnerEditTextNotClickable() {
-        mEditText.setClickable(false);
-        mEditText.setFocusable(false);
-        mEditText.setFocusableInTouchMode(false);
-        mEditText.setMovementMethod(null);
-        mEditText.setKeyListener(null);
-    }
-
     public void setText(String text) {
-        mEditText.setText(text);
+        mTextView.setText(text);
     }
     public void setText(@StringRes int textId) {
         if (textId != -1)
-            mEditText.setText(textId);
+            mTextView.setText(textId);
     }
     public String getText() {
-        return mEditText.getText().toString();
+        return mTextView.getText().toString();
     }
 
 
     public void setHint(String hint) {
-        mEditText.setHint(hint);
+        mTextView.setHint(hint);
     }
     public void setHint(@StringRes int hintId) {
         if (hintId != -1)
-            mEditText.setHint(hintId);
-    }
-
-    public void addTextChangedListener(TextWatcher tw) {
-        mEditText.addTextChangedListener(tw);
-    }
-
-
-    public void setFilters(InputFilter[] filters) {
-        mEditText.setFilters(filters);
+            mTextView.setHint(hintId);
     }
 
     public void setDim(boolean dim) {
         if(dim) {
             setBackgroundResource(R.drawable.round_edges_background_dim);
-            mEditText.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_background));
+            mTextView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_background));
 
             mIcon.setColorFilter(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_foreground), android.graphics.PorterDuff.Mode.MULTIPLY);
-            mEditText.setTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_foreground));
-            mEditText.setHintTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_foreground));
+            mTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_foreground));
+            mTextView.setHintTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_dim_foreground));
         } else {
             setBackgroundResource(R.drawable.round_edges_background);
-            mEditText.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_background));
+            mTextView.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_background));
 
             mIcon.setColorFilter(ContextCompat.getColor(getContext(), R.color.custom_edit_text_icon), android.graphics.PorterDuff.Mode.MULTIPLY);
-            mEditText.setTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_text));
-            mEditText.setHintTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_hint));
+            mTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_text));
+            mTextView.setHintTextColor(ContextCompat.getColor(getContext(), R.color.custom_edit_text_hint));
         }
     }
 }
